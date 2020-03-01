b34fe1686451fce2ef5c93916cee7c5c3d8e9deb
KParrish01 <klaraparrish@yahoo.com>
2020-03-01 12:19:25 -0600

    Part 1 done.

--- queries.sql
+++ queries.sql
@@ -1,5 +1,13 @@
 ## Part 1: Test it with SQL
 
+CREATE TABLE job (
+id int PK /* id INTEGER PRIMARY KEY (not AUTO_INCREMENT within MySQL */,
+employer VARCHAR(255),
+name VARCHAR(255),
+skills VARCHAR(255)
+);
+
+
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/JavaWebDevTechjobsPersistentApplication.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/JavaWebDevTechjobsPersistentApplication.java
@@ -8,6 +8,7 @@ public class JavaWebDevTechjobsPersistentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaWebDevTechjobsPersistentApplication.class, args);
+		System.out.println("Ready to go!");
 	}
 
 }
